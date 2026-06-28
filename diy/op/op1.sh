ad8f668a17f30ad928cf06e138ea0a19e7d9585a
GXLCLC <2193160597@qq.com>
2026-06-28 20:03:00 +0800

    Delete diy/op directory

--- diy/op/op1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#=================================================
-# DaoDao's script
-#=================================================
-##添加自己的插件库
-sed -i 's/git.openwrt.org\/feed/github.com\/openwrt/g' feeds.conf.default
-sed -i 's/git.openwrt.org\/project/github.com\/openwrt/g' feeds.conf.default
-
-sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
